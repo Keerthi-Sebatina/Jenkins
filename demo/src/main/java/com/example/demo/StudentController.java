92e80737ff935ccf108acbfa1484017aec35d371
Keerthi-Sebatina <keerthisebatina.27csa@licet.ac.in>
2026-04-01 09:29:11 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/StudentController.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class StudentController {
+
+    public void printGreeting(String studentName) {
+        System.out.println("Hello, " + studentName + "! Welcome to the Student System.");
+    }
+}
