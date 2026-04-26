8fac3248e02ad6e5d59f8645cfc3ece79b883a7e
Keerthi-Sebatina <keerthisebatina.27csa@licet.ac.in>
2026-04-26 17:20:53 +0530

    Update greeting message in printGreeting method

--- demo/src/main/java/com/example/demo/StudentController.java
+++ demo/src/main/java/com/example/demo/StudentController.java
@@ -3,6 +3,6 @@ package com.example.demo;
 public class StudentController {
 
     public void printGreeting(String studentName) {
-        System.out.println("Hello, " + studentName + "! Welcome to the Student System.");
+        System.out.println("Hello, " + studentName + "! Welcome to the Student System Portal");
     }
 }
